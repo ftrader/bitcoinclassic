8690cd48df587a31041533a6ce854f1fa3f5e51a
TomZ <tomz@freedommail.ch>
2017-07-24 00:01:15 +0200

    Add anti-replay feature based on OP_RETURN for uahf

--- src/main.cpp
+++ src/main.cpp
@@ -3134,11 +3134,23 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
         const int64_t startTime = Application::uahfStartTime();
         if (pindexPrev->GetMedianTimePast() >= startTime) {
             // this is a UAHF-chain block!
+
             if (pindexPrev->pprev->GetMedianTimePast() < startTime) {
                 // If UAHF is enabled for the curent block, but not for the previous
                 // block, we must check that the block is larger than 1MB.
                  if (blockSize <= MAX_BLOCK_SIZE)
                      return state.DoS(100, false, REJECT_INVALID, "bad-blk-too-small", false, "size limits failed");
+            }
+
+            if (nHeight <= consensusParams.antiReplayOpReturnSunsetHeight) {
+                for (const CTransaction &tx : block.vtx) {
+                    for (const CTxOut &o : tx.vout) {
+                        if (o.scriptPubKey.isCommitment(consensusParams.antiReplayOpReturnCommitment)) {
+                            return state.DoS(10, false, REJECT_INVALID, "bad-txn-replay",
+                                             false, "non playable transaction");
+                        }
+                    }
+                }
             }
         }
     }
