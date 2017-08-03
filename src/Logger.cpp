e55c3fe9d0d5f43d0bf9ee650464666d369cad64
TomZ <tomz@freedommail.ch>
2017-08-03 12:56:56 +0200

    fix logic.
    Thanks fly to @KyrosKrane for finding this!

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -122,7 +122,7 @@ short Log::Manager::section(const char *category)
         return Log::Global;
     auto iter = d->categoryMapping.find(category);
     assert (iter != d->categoryMapping.end());
-    if (iter == d->categoryMapping.end())
+    if (iter != d->categoryMapping.end())
         return iter->second;
     return Log::Global;
 }
