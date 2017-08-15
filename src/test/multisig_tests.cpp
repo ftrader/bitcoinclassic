07c355928813a4de2e54c0fdc0f2f132fb936d9e
gubatron <gubatron@gmail.com>
2017-08-15 11:16:34 -0600

    [issue-210] removed 'using namespace std' from multisig_tests.cpp
    Also unused includes cleanup

--- src/test/multisig_tests.cpp
+++ src/test/multisig_tests.cpp
@@ -2,14 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "key.h"
 #include "keystore.h"
 #include "policy/policy.h"
-#include "script/script.h"
-#include "script/script_error.h"
-#include "script/interpreter.h"
 #include "script/sign.h"
-#include "uint256.h"
 #include "test/test_bitcoin.h"
 
 #ifdef ENABLE_WALLET
@@ -19,14 +14,12 @@
 #include <boost/foreach.hpp>
 #include <boost/test/unit_test.hpp>
 
-using namespace std;
-
-typedef vector<unsigned char> valtype;
+typedef std::vector<unsigned char> valtype;
 
 BOOST_FIXTURE_TEST_SUITE(multisig_tests, BasicTestingSetup)
 
 CScript
-sign_multisig(CScript scriptPubKey, vector<CKey> keys, CTransaction transaction, int whichIn)
+sign_multisig(CScript scriptPubKey, std::vector<CKey> keys, CTransaction transaction, int whichIn)
 {
     uint256 hash = SignatureHash(scriptPubKey, transaction, whichIn, 0, SIGHASH_ALL);
 
@@ -34,7 +27,7 @@ sign_multisig(CScript scriptPubKey, vector<CKey> keys, CTransaction transaction,
     result << OP_0; // CHECKMULTISIG bug workaround
     BOOST_FOREACH(const CKey &key, keys)
     {
-        vector<unsigned char> vchSig;
+        std::vector<unsigned char> vchSig;
         BOOST_CHECK(key.Sign(hash, vchSig));
         vchSig.push_back((unsigned char)SIGHASH_ALL);
         result << vchSig;
@@ -77,7 +70,7 @@ BOOST_AUTO_TEST_CASE(multisig_verify)
         txTo[i].vout[0].nValue = 1;
     }
 
-    vector<CKey> keys;
+    std::vector<CKey> keys;
     CScript s;
 
     // Test a AND b:
@@ -202,7 +195,7 @@ BOOST_AUTO_TEST_CASE(multisig_Solver1)
     partialkeystore.AddKey(key[0]);
 
     {
-        vector<valtype> solutions;
+        std::vector<valtype> solutions;
         txnouttype whichType;
         CScript s;
         s << ToByteVector(key[0].GetPubKey()) << OP_CHECKSIG;
@@ -217,7 +210,7 @@ BOOST_AUTO_TEST_CASE(multisig_Solver1)
 #endif
     }
     {
-        vector<valtype> solutions;
+        std::vector<valtype> solutions;
         txnouttype whichType;
         CScript s;
         s << OP_DUP << OP_HASH160 << ToByteVector(key[0].GetPubKey().GetID()) << OP_EQUALVERIFY << OP_CHECKSIG;
@@ -232,7 +225,7 @@ BOOST_AUTO_TEST_CASE(multisig_Solver1)
 #endif
     }
     {
-        vector<valtype> solutions;
+        std::vector<valtype> solutions;
         txnouttype whichType;
         CScript s;
         s << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
@@ -247,13 +240,13 @@ BOOST_AUTO_TEST_CASE(multisig_Solver1)
 #endif
     }
     {
-        vector<valtype> solutions;
+        std::vector<valtype> solutions;
         txnouttype whichType;
         CScript s;
         s << OP_1 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
         BOOST_CHECK(Solver(s, whichType, solutions));
         BOOST_CHECK_EQUAL(solutions.size(), 4U);
-        vector<CTxDestination> addrs;
+        std::vector<CTxDestination> addrs;
         int nRequired;
         BOOST_CHECK(ExtractDestinations(s, whichType, addrs, nRequired));
         BOOST_CHECK(addrs[0] == keyaddr[0]);
@@ -266,7 +259,7 @@ BOOST_AUTO_TEST_CASE(multisig_Solver1)
 #endif
     }
     {
-        vector<valtype> solutions;
+        std::vector<valtype> solutions;
         txnouttype whichType;
         CScript s;
         s << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << ToByteVector(key[2].GetPubKey()) << OP_3 << OP_CHECKMULTISIG;
